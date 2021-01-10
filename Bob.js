24b5232b6654cbb732f12ca0ca667a6348c88f38
Ashnasmitha <75996347+Ashnasmitha@users.noreply.github.com>
2021-01-10 18:34:00 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,20 @@
+class Bob
+{
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.body=Bodies.circle(x,y,40,options);
+        World.add(world,this.body);
+      
+    }
+    display()
+    {
+        ellipseMode(RADIUS);
+        fill ("deepPink");
+        ellipse(this.body.position.x,this.body.position.y,40,40); 
+    }
+}
